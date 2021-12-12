2b0a13aee8580961b22f87fd13b4db0262984403
Albert Hu <albert@alchemy.com>
2021-12-11 22:56:22 -0800

    download and compile chainrunners code

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,5 +2,20 @@
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.4",
+  solidity: {
+    version:  "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 2000,
+        details: {
+          yul: true,
+          yulDetails: {
+            stackAllocation: true,
+            optimizerSteps: "dhfoDgvulfnTUtnIf"
+          }
+        }
+      }
+    },
+  },
 };
